928b3f1741b044106eba9b9caadb540b09dfefea
Leftddr <qhddnjs75@gmail.com>
2020-11-08 00:29:15 +0900

    path 수정

--- posenet/converter/config.py
+++ posenet/converter/config.py
@@ -5,6 +5,7 @@ BASE_DIR = os.path.dirname(__file__)
 
 
 def load_config(config_name='config.yaml'):
-    cfg_f = open(os.path.join(BASE_DIR, config_name), "r+")
+    #cfg_f = open(os.path.join(BASE_DIR, config_name), "r+")
+    cfg_f = open(BASE_DIR + '/' + config_name, "r")
     cfg = yaml.load(cfg_f)
     return cfg
